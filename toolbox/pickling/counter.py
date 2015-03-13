236c530ae574925abbb289d423c69fc98469331a
Bill Wong <billmwong@gmail.com>
2015-03-13 13:12:56 -0400

    Finished counter.py

--- toolbox/pickling/counter.py
+++ toolbox/pickling/counter.py
@@ -1,4 +1,7 @@
-""" A program that stores and updates a counter using a Python pickle file"""
+""" A program that stores and updates a counter using a Python pickle file
+
+@author: Bill Wong
+"""
 
 from os.path import exists
 import sys
@@ -29,7 +32,14 @@ def update_counter(file_name, reset=False):
 	>>> update_counter('blah2.txt')
 	2
 	"""
-	pass
+	if exists(file_name) == True and reset == False:
+		counter = load(open(file_name, 'r+')) + 1
+		dump(counter, open(file_name, 'r+'))
+		return counter
+	elif exists(file_name) == False or reset == True:
+		counter = 1
+		dump(counter, open(file_name, "w"))
+		return counter
 
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
